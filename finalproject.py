005f9973494a63d5680a1c88778439f57131d71e
Brady Lavoie <bflavoie@wpi.edu>
2025-12-10 21:50:02 -0500

    Successfully Import Training Data as Tensor

--- finalproject.py
+++ finalproject.py
@@ -1,27 +1,57 @@
 import torch
-import tf 
+import torchvision
+from torchvision import transforms
+import tensorflow as tf
 import numpy as np 
 from PIL import Image
+import os
+
+def load_data(type):
+
+    folders = ["finger_gun", "high_five", "okay", "peace", "thumbs_up"]
+
+    images = []
+    labels = []
+
+    resize_tf = transforms.Resize(size=(224, 224))
+
+    for folder in folders:
+
+        folder_path = "data/" + type + "/" + folder
+
+        for filename in os.listdir(folder_path):
+
+            filepath = os.path.join(folder_path, filename)
+
+            image = resize_tf(Image.open(filepath))
+            image = image.rotate(-90, expand=True)       # Resize order is of opposite order (swapped height and width), needs rotation back
+
+            images.append(image)
+            labels.append(folder)
+
+    
+    images[10].save("debug.png")
+
+    return np.array(images), np.array(labels)
 
 def main():
-    # Load a sample image using PIL
-    image_path = 'sample_image.jpg'
-    image = Image.open(image_path).convert('RGB')
-    
-    # Convert the image to a NumPy array
-    image_np = np.array(image)
+
+    images_tr, labels_tr = load_data("train")
     
     # Convert the NumPy array to a TensorFlow tensor
-    image_tf = tf.convert_to_tensor(image_np, dtype=tf.float32)
+    image_tf = tf.convert_to_tensor(images_tr, dtype=tf.float32)
     
     # Normalize the TensorFlow tensor
     image_tf = image_tf / 255.0
     
     # Convert the TensorFlow tensor to a PyTorch tensor
-    image_torch = torch.from_numpy(image_tf.numpy()).permute(2, 0, 1)  # Change from HWC to CHW format
+    image_torch = torch.from_numpy(image_tf.numpy()).permute(0, 3, 1, 2)
+
+    torchvision.utils.save_image(image_torch, "debug2.png")
     
     # Print the shapes of the tensors
-    print(f'PIL Image size: {image.size}')
-    print(f'NumPy array shape: {image_np.shape}')
     print(f'TensorFlow tensor shape: {image_tf.shape}')
     print(f'PyTorch tensor shape: {image_torch.shape}')
+
+if __name__ == "__main__":
+    main()
