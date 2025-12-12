1386ea7025593929c9fabb07c58f95d2ab220e78
Brady Lavoie <bflavoie@wpi.edu>
2025-12-11 23:40:46 -0500

    Working

--- finalproject.py
+++ finalproject.py
@@ -2,12 +2,11 @@ import torch
 import torchvision
 from torchvision import transforms, models
 from torchvision.transforms import functional
-from torchvision.datasets import ImageFolder
-from torch.utils.data import DataLoader
 import numpy as np 
 import os
 from torchmetrics import Accuracy, Precision, Recall, F1Score
 from torchmetrics.classification import MulticlassAccuracy
+from torchvision.models import ResNet18_Weights
 
 def load_data(type):
 
@@ -16,7 +15,8 @@ def load_data(type):
     images = []
     labels = []
 
-    resize_tf = transforms.Resize(size=(224, 224))      # Images need to be resized for compatibility with ResNet18
+    # Images need to be resized and normalized for compatibility with ResNet18
+    resize_tf = transforms.Resize(size=(224, 224))
     normalize_tf = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 
     for label_idx, folder in enumerate(folders):
@@ -41,21 +41,28 @@ def load_data(type):
 
 def build_resnet_model(num_classes):    
     # pretrained model
-    model = models.resnet18(pretrained=True)
+    weights = ResNet18_Weights.DEFAULT
+    model = models.resnet18(weights=weights)
     # change last layer to match our classes from data
+    
+    # TEMP
+    for param in model.parameters():
+        param.requires_grad = False
+    
     features = model.fc.in_features
     model.fc = torch.nn.Linear(features, num_classes)
     
     return model
 
 # based default values off of the last homework, must hyperparamater tune
-def train(images, labels, model, num_epochs=100, learning_rate=1e-4, batch_size=10):
+# Good at all but finger gun (unfrozen layers): epoch=25 rate=1e-4 batch_size = 10
+def train(images, labels, model, num_epochs=2000, learning_rate=2e-3, batch_size=4):
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(f"Using: {device}")
     model.to(device)
     criterion = torch.nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=1e-4)
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=1e-3)
 
     for epoch in range(num_epochs):
         for i in range(0, len(images), batch_size):
@@ -75,14 +82,12 @@ def train(images, labels, model, num_epochs=100, learning_rate=1e-4, batch_size=
     print("Saved PyTorch Model State to model.pth")
 
 # model is resnet model
-# test_loader is DataLoader object with test data
-# device is the device being run on (defined in train)
 def test(model, images, labels):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     num_classes = 5
 
-    accuracy = MulticlassAccuracy(num_classes=num_classes).to(device)
+    accuracy = MulticlassAccuracy(num_classes=num_classes, average=None).to(device)
     precision = Precision(task="multiclass", num_classes=num_classes).to(device)
     recall = Recall(task="multiclass", num_classes=num_classes).to(device)
     f1 = F1Score(task="multiclass", num_classes=num_classes).to(device)
@@ -90,36 +95,29 @@ def test(model, images, labels):
     model.eval()
 
     with torch.no_grad():
-        for images, labels in images, labels:
 
-            inputs = inputs.to(device)
-            labels = labels.to(device)
+        inputs = images.to(device)
+        labels = labels.to(device)
 
-            outputs = model(inputs)
-            preds = torch.argmax(outputs, dim=1)
+        outputs = model(inputs)
+        preds = torch.argmax(outputs, dim=1)
 
-            accuracy.update(preds,labels)
-            precision.update(preds, labels)
-            recall.update(preds, labels)
-            f1.update(preds, labels)
+        print("True dist: ", torch.bincount(labels, minlength=num_classes))
+        print("Pred dist: ", torch.bincount(preds, minlength=num_classes))
+
+        accuracy.update(preds, labels)
     
     acc_per_class = accuracy.compute().cpu().numpy()
-    prec_per_class = precision.compute().cpu().numpy()
-    rec_per_class = recall.compute().cpu().numpy()
-    f1_per_class = f1.compute().cpu().numpy()
 
-    return acc_per_class, prec_per_class, rec_per_class, f1_per_class
+    return acc_per_class
 
 #def visualize_g_x(model, )
-
-def predict(image_tensor, class_names):
-    return
 
 def main():
 
     images_tr, labels_tr = load_data("train")
 
-    torchvision.utils.save_image(images_tr, "debug2.png")
+    torchvision.utils.save_image(images_tr, "debug_train.png")
     
     # Print the shapes of the tensors
     print(f'PyTorch tensor shape: {images_tr.shape}')
@@ -130,8 +128,9 @@ def main():
 
     images_te, labels_te = load_data("test")
     
-    test(model, images_te, labels_te)
+    torchvision.utils.save_image(images_te, "debug_test.png")
 
+    print(f"Testing accuracy = {test(model, images_te, labels_te)}")
 
 if __name__ == "__main__":
 
